b402a097bd537628aee9bff9449d108ba3ea1e16
John Serrano <andrey_201015@hotmail.com>
2019-08-02 10:55:46 -0500

    Manipulando el history

--- src/pages/components/not-found.js
+++ src/pages/components/not-found.js
@@ -2,12 +2,33 @@ import React, { PureComponent } from'react';
 import'./generic-page.css';
 
 class NotFound extends PureComponent {
+
+  handleForward = () => {
+    this.props.history.goForward()
+  }
+  handleBack = () => {
+    // this.props.history.goBack()
+    this.props.history.go(-1)
+  }
+  handleVideo = () => {
+    const random = Math.round(Math.random() * (10 - 1) + 1);
+    this.props.history.push(`/videos?id=${random}`, { id: random})
+  }
   render() {
     return (
       <div className="Page NotFound">
         <h1>404</h1>
         <h3 className="SadFace">:(</h3>
         <h2>No hemos encontrado la página que buscabas</h2>
+        <button onClick={this.handleForward}>
+          Ir a la siguiente ruta
+        </button>
+        <button onClick={this.handleBack}>
+          Ir a la anterior ruta
+        </button>
+        <button onClick={this.handleVideo}>
+          Ir a video random
+        </button>
       </div>
     )
   }
--- src/pages/containers/Videos.js
+++ src/pages/containers/Videos.js
@@ -29,6 +29,14 @@ class Home extends Component {
     this.props.history.goBack()
     this.props.actions.closeModal()
   }
+
+  componentDidMount() {
+    const search = this.props.location.search;
+    if (search) {
+      const id = search.split('=')[1]
+      this.handleOpenModal(id);
+    }
+  }
   render() {
     return (
       <HandleError>
